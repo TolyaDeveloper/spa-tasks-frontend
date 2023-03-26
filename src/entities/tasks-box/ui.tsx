5f04fb7314f9a5f97a94376e709e553df99b17c7
Anatolii <kabantolyan@gmail.com>
2023-03-26 22:36:53 +0300

    tasks box

--- src/entities/tasks-box/ui.tsx
+++ src/entities/tasks-box/ui.tsx
@@ -1,5 +1,11 @@
-import { BoxWrapper } from '~/shared/ui'
+import { BoxWrapper, BoxTitle } from '~/shared/ui'
+import { FillIcon } from '~/shared/icons'
 
 export const TasksBox = () => {
-  return <BoxWrapper>TasksBox</BoxWrapper>
+  return (
+    <BoxWrapper>
+      <BoxTitle title="Active tasks" rightIcon={<FillIcon icon="DotsIcon" />} />
+      <BoxTitle title="Finished tasks" />
+    </BoxWrapper>
+  )
 }
